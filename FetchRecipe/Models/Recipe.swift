6c5bcefbaec24859439fd3d0ba8559dde92a3c96
bnicoloulias <bnicoloulias@gmail.com>
2025-02-07 12:44:24 -0500

    Created Recipe struct

--- FetchRecipe/Models/Recipe.swift
+++ FetchRecipe/Models/Recipe.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Recipe: Identifiable {
+    let id: UUID
+    var cuisine: String
+    var name: String
+    var photoUrlLarge: String?
+    var photoUrlSmall: String?
+    var sourceUrl: String?
+    var youtubeUrl: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case cuisine
+        case name
+        case photoUrlLarge = "photo_url_large"
+        case photoUrlSmall = "photo_url_small"
+        case sourceUrl = "source_url"
+        case youtubeUrl = "youtube_url"
+    }
+}
