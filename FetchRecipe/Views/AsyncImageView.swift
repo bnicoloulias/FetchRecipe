27a87b90db4c9a80ac8bec6765c7af8c5128d3dc
bnicoloulias <bnicoloulias@gmail.com>
2025-02-08 15:09:43 -0500

    Created AsyncImageView

--- FetchRecipe/Views/AsyncImageView.swift
+++ FetchRecipe/Views/AsyncImageView.swift
@@ -6,13 +6,31 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AsyncImageView: View {
+    @StateObject private var imageLoader: ImageLoader
+    
+    init(url: URL, context: NSManagedObjectContext) {
+        _imageLoader = StateObject(wrappedValue: ImageLoader(url: url, context: context))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if let uiImage = imageLoader.image {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                ProgressView()
+            }
+        }
+        .task {
+            await imageLoader.loadImage()
+        }
     }
 }
 
-#Preview {
-    AsyncImageView()
-}
+//#Preview {
+//    AsyncImageView()
+//}
