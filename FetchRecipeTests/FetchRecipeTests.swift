3ff83a7e94fb1424d780b76e837c95345a74a2ca
bnicoloulias <bnicoloulias@gmail.com>
2025-02-09 13:53:30 -0500

    Created tests for network  json types [success, malformed, empty]

--- FetchRecipeTests/FetchRecipeTests.swift
+++ FetchRecipeTests/FetchRecipeTests.swift
@@ -9,9 +9,38 @@ import Testing
 @testable import FetchRecipe
 
 struct FetchRecipeTests {
+    var recipeViewModel: RecipeViewModel!
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    mutating func setup(with jsonType: JsonType) async {
+        let mockNetworkService = MockNetworkService(jsonType: jsonType)
+        recipeViewModel = await RecipeViewModel(networkService: mockNetworkService)
     }
 
+    @Test mutating func testFetchRecipesJsonSuccess() async throws {
+        await setup(with: .success)
+
+        await recipeViewModel.fetchRecipes()
+
+        await #expect(recipeViewModel.filteredRecipes.count == 63)
+        await #expect(recipeViewModel.filteredRecipes.first?.name == "Apam Balik")
+        await #expect(recipeViewModel.errorDescription == nil)
+    }
+    
+    @Test mutating func testFetchRecipesJsonMalformed() async throws {
+        await setup(with: .malformed)
+
+        await recipeViewModel.fetchRecipes()
+
+        await #expect(recipeViewModel.filteredRecipes.isEmpty)
+        await #expect(recipeViewModel.errorDescription != nil)
+    }
+    
+    @Test mutating func testFetchRecipesJsonEmpty() async throws {
+        await setup(with: .empty)
+
+        await recipeViewModel.fetchRecipes()
+
+        await #expect(recipeViewModel.filteredRecipes.isEmpty)
+        await #expect(recipeViewModel.errorDescription == nil)
+    }
 }
